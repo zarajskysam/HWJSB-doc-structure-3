ed86c1ce373b6bfb0550ec007ed39862928145a4
Semyon Zarajsky <zarajskysam92@gmail.com>
2020-08-13 22:47:30 +0300

    commit added 2

--- task.js
+++ task.js
@@ -13,26 +13,17 @@ for (let i = 0; i < product.length; i++) {
                 event.target.nextElementSibling.textContent = 0;
             }
         } else if (event.target.classList.contains("product__add")){
-            if (cartProduct.length == 0){
-                cartProducts[0].innerHTML += `<div class="cart__product" data-id="${product[i].getAttribute('data-id')}">
-                                        <img class="cart__product-image" src="${product[i].children[1].src}">
-                                        <div class="cart__product-count">${event.target.previousElementSibling.children[1].textContent}</div>
-                                        </div>`;
-            } else if (cartProduct.length > 0) {
-                for (let j = 0; j < cartProduct.length; j++){
-                    if (product[i].getAttribute('data-id') == cartProduct[j].getAttribute('data-id')){
-                        cartProduct[j].children[1].textContent = +event.target.previousElementSibling.children[1].textContent + parseInt(cartProduct[j].children[1].textContent);
-                    } else if (product[i].getAttribute('data-id') != cartProduct[j].getAttribute('data-id')){
-                        cartProducts[0].innerHTML += `<div class="cart__product" data-id="${product[i].getAttribute('data-id')}">
-                            <img class="cart__product-image" src="${product[i].children[1].src}">
-                            <div class="cart__product-count">${event.target.previousElementSibling.children[1].textContent}</div>
-                            </div>`;
-                    }
-                }
-                
-            }   
-            
-        } 
+            let productInCart = document.querySelector(`.cart__product[data-id='${product[i].getAttribute("data-id")}']`);
+    
+            if (productInCart) {
+                productInCart.querySelector('.cart__product-count').textContent = +productInCart.querySelector('.cart__product-count').textContent + parseInt(event.target.previousElementSibling.children[1].textContent);
+            } else {
+                cartProducts[0].innerHTML += `<div class="cart__product" data-id="${product[i].getAttribute("data-id")}">
+                                                <img class="cart__product-image" src="${product[i].children[1].getAttribute("src")}">
+                                                <div class="cart__product-count">${event.target.previousElementSibling.children[1].textContent}</div>
+                                            </div>`;
+            }
+        }
     });
 };
 
